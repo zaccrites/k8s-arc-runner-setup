9b71084fd4a7bc0a01f54f8b4940282114540330
Zac Crites <zac@criteshouse.com>
2026-03-15 11:24:25 -0600

    Add Docker-in-Docker ARC runner set

--- github_arc/install.sh
+++ github_arc/install.sh
@@ -8,16 +8,24 @@ set -x
 
 
 # https://docs.github.com/en/actions/tutorials/use-actions-runner-controller/quickstart
-helm install arc \
+helm upgrade --install arc \
     --namespace "arc-systems" \
     --create-namespace \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
 
 
 # https://github.com/actions/actions-runner-controller/blob/master/charts/gha-runner-scale-set/values.yaml
-helm install arc-runner-set \
+helm upgrade --install arc-runner-set \
     --namespace "arc-runners" \
     --create-namespace \
     --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
     --set githubConfigSecret.github_token="${GITHUB_TOKEN}" \
+    oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+
+helm upgrade --install arc-runner-set-dind \
+    --namespace "arc-runners" \
+    --create-namespace \
+    --set githubConfigUrl="${GITHUB_CONFIG_URL}" \
+    --set githubConfigSecret.github_token="${GITHUB_TOKEN}" \
+    --set containerMode.type=dind \
     oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
